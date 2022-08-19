afdd28dca0c5ebcd442cabcff529e566e8ee4bad
EZEN-141 <EZEN-141@DESKTOP-9TO3VUH>
2022-08-19 20:19:36 +0900

    개발 환경 깃허브 반영

---
+++ 8projectRecoveryPsu/src/projectRecoveryPsu/projectRecoveryTest.java
@@ -0,0 +1,14 @@
+package projectRecoveryPsu;
+
+public class projectRecoveryTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("주어진 자료를 기반으로 복원하여");
+		System.out.println("개발 환경을 구축할 수 있다");
+		
+		
+	}
+
+}
